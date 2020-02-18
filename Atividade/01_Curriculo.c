1ddd9ee50fb2037c56ba61f8d3b9914f662ea0f6
Mitsu325 <patricia.mashiba@gmail.com>
2020-02-18 10:35:34 -0300

    Inclusão das acentuações

--- Atividade/01_Curriculo.c
+++ Atividade/01_Curriculo.c
@@ -2,20 +2,17 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<locale.h>
 
 int main () {
 	
-	setLocale(LC_ALL, "Portuguese_Brazil");
-	
 	printf("\n\t------------------------------------------------------------\n");
-	printf("\t\t\t\t** Curriculo **");
+	printf("\t\t\t\t** Curr%cculo **", 161);
 	printf("\n\t------------------------------------------------------------\n");
-	printf("\n\tNome: Patricia Mitsuko Mashiba");
+	printf("\n\tNome: Patr%ccia Mitsuko Mashiba", 161);
 	printf("\n\tIdade: 22 anos");
-	printf("\n\tFormacao: Estudante de Analise e Desenvolvimento de Sistemas");
-	printf("\n\tProfissao: Tecnologa");
-	printf("\n\tTempo de experiencia: 7 meses\n\n");
+	printf("\n\tForma%cao: Estudante de An%clise e Desenvolvimento de Sistemas", 135, 160);
+	printf("\n\tProfissao: Tecn%cloga", 162);
+	printf("\n\tTempo de experi%cncia: 7 meses\n\n", 136);
 	
 	system ("pause");
 	return 0;
