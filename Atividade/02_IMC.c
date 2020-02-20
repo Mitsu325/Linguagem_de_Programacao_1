2e460e4f6f992946e03445c472e5bd3b34ae4d01
Mitsu325 <patricia.mashiba@gmail.com>
2020-02-20 16:23:17 -0300

    Primeira versão feitogit add . A função não está calculando o imc.

--- Atividade/02_IMC.c
+++ Atividade/02_IMC.c
@@ -1,3 +1,13 @@
 /*Atividade 2 - IMC */
 
+#include<stdio.h>
+#include<stdlib.h>
 
+int main() {
+	float imc, peso, altura;
+	
+	printf("\n\tDigite o peso e a altura: ");
+	scanf(" %f, %f", &peso, &altura);
+	imc = peso / (altura * altura);
+	printf("%f", &imc);
+}
