c5462cd6a98fef41fbc1a8c484a1ef6b88c97ce1
Mitsu325 <patricia.mashiba@gmail.com>
2020-02-21 10:29:06 -0300

    Inserção dos dados peso e altura em scanf separado e correção do erro no print de exibição do resultado final (excluído o símbolo

--- Atividade/02_IMC.c
+++ Atividade/02_IMC.c
@@ -6,8 +6,17 @@
 int main() {
 	float imc, peso, altura;
 	
-	printf("\n\tDigite o peso e a altura: ");
-	scanf(" %f, %f", &peso, &altura);
+	printf("\n\t-------*Programa calcula IMC*-------\n");
+	
+	printf("\n\tDigite o seu peso em kg: ");
+		scanf("%f", &peso);
+	printf("\n\tDigite a sua altura em metros: ");
+		scanf("%f", &altura);
+	
 	imc = peso / (altura * altura);
-	printf("%f", &imc);
+	
+	printf("\n\tO seu IMC %c %.2f\n\n", 130, imc);
+	
+	system ("pause");
+	return 0;
 }
