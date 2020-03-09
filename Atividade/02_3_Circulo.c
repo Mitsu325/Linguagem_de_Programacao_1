8ac68be19bc19535ee8013ed5adfa4b113c98ffa
Mitsu325 <patricia.mashiba@gmail.com>
2020-03-09 10:14:15 -0300

    Atividade 2-3 Círculo. Calcular área e perímetro

---
+++ Atividade/02_3_Circulo.c
@@ -0,0 +1,25 @@
+/*Atividade 2.3 - Circunferência*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	const float PI = 3.1415;
+	float raio, area, perimetro;
+	system("cls");
+	
+	printf("\n\t-------*Programa calcula %crea e per%cmetro do c%crculo*-------\n", 160, 161, 161);
+	
+	printf("\n\tDigite o raio: ");
+		scanf("%f", &raio);
+	
+	area = PI * pow(raio, 2);
+	perimetro = 2 * PI * raio;
+	
+	printf("\n\tA %crea %c %.2f", 160, 130, area);
+	printf("\n\tO per%cmetro %c %.2f \n", 161, 130, perimetro);
+	
+	system ("pause");
+	return 0;
+}
+
