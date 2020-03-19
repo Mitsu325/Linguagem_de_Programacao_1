4b8488ce3de154e83715a91269ea84d252053d5a
Mitsu325 <patricia.mashiba@gmail.com>
2020-03-19 10:29:24 -0300

    Renomeação dos Arquivos

--- Atividade/02_IMC.c
+++ Atividade/02_IMC.c
@@ -1,19 +1,18 @@
-/*Atividade 2 - IMC */
+/*Atividade 2.1 - IMC*/
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
 int main() {
 	float imc, peso, altura;
+	system("cls");
 	
 	printf("\n\t-------*Programa calcula IMC*-------\n");
 	
-	printf("\n\tDigite o seu peso em kg: ");
-		scanf("%f", &peso);
-	printf("\n\tDigite a sua altura em metros: ");
-		scanf("%f", &altura);
+	printf("\n\tDigite o peso e altura: ");
+		scanf("%f%f", &peso, &altura);
 	
-	imc = peso / (altura * altura);
+	imc = peso / pow(altura, 2);
 	
 	printf("\n\tO seu IMC %c %.2f\n\n", 130, imc);
 	
